f68be446438ac628c34170e0c6cf9a0b1e1f5381
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-08-16 14:45:52 +0900

    #5 - 결과 표시

--- lib/main.dart
+++ lib/main.dart
@@ -112,6 +112,9 @@ class BmiResult extends StatelessWidget {
   BmiResult(this.height,this.weight);
   @override
   Widget build(BuildContext context) {
+    final bmi = weight / ((height/100) * (height/100));
+    print('bmi: $bmi');
+
     return Scaffold(
       appBar: AppBar(
         title: Text('비만도 계산기'),
@@ -121,21 +124,53 @@ class BmiResult extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '정상',
+              _calcBmi(bmi),
               style: TextStyle(fontSize: 50),
             ),
             SizedBox(
               height: 16,
             ),
-            Icon(
-              Icons.sentiment_satisfied,
-              color: Colors.green,
-              size: 100,
-            ),
+            _buildIcon(bmi),
           ],
         ),
       ),
     );
+  }
+  String _calcBmi(double bmi) {
+    var result = '저체중';
+    if(bmi>=35) {
+      result = '고도비만';
+    } else if(bmi>=30) {
+      result = '2단계 비만';
+    } else if(bmi>=25) {
+      result = '1단계 비만';
+    } else if(bmi>=23) {
+      result = '과체중';
+    } else if(bmi>=18.5) {
+      result = '정상';
+    }
+    return result;
+  }
+  Widget _buildIcon(double bmi) {
+    if(bmi >= 23) {
+      return Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.red,
+        size: 100,
+      );
+    } else if(bmi >= 18.5) {
+      return Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    } else {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.orange,
+        size: 100,
+      );
+    }
   }
 }
 
